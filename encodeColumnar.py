9ad2f471da468d9eb449e59c0affdcfb8ef4ac5f
Daniel Chen <dchen50@stuy.edu>
2025-05-20 14:47:25 -0400

    working on print stuff

--- encodeColumnar.py
+++ encodeColumnar.py
@@ -18,5 +18,16 @@ def column(plaintext, keyword):
             break
     return ciphertext
 
+def columnPrint(doubleArray):
+    string = ""
+    for i in range(0, len(doubleArray[0])):
+        for j in range(0, len(doubleArray)):
+            if i < len(doubleArray[j]):
+                string += doubleArray[j][i]
+            else:
+                break
+        string+="\n"
+    return string
+
 if __name__ == "__main__":
-    print(column(sys.argv[1], sys.argv[2]))
+    columnPrint(column(sys.argv[1], sys.argv[2]))
