f527f19e0bd0ad5e602d969b23f1391f1ca72b06
Stella Yampolsky <stellagraceyampolksky@gmail.com>
2025-05-26 15:03:50 -0400

    encrypt

--- encodeHill.py
+++ encodeHill.py
@@ -5,3 +5,14 @@ def textToNums(text):
 
 def numsToText(nums):
     return ''.join(chr(n % 26 + ord('A')) for n in nums)
+
+def encrypt(text, key):
+    nums = textToNums(text)
+    if len(nums) % 2 != 0:
+        nums.append(0)
+    result = []
+    for i in range(0, len(nums), 2):
+        pair = np.array(nums[i:i+2]).reshape(2, 1)
+        enc = key @ pair % 26
+        result.extend(enc.flatten())
+    return numsToText(result)
